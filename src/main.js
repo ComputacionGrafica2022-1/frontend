f37a69c1ffd9a55c7d7a0f733016766f36a2375b
Diego <dfsanchezme@gmail.com>
2022-06-26 11:31:32 -0500

    Changing API url

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ import NowUiKit from './plugins/now-ui-kit';
 Vue.config.productionTip = false;
 
 Vue.use(NowUiKit);
-Vue.prototype.$apiURL = " https://17ed-20-64-114-63.ngrok.io"
+Vue.prototype.$apiURL = "https://b4b8-20-64-114-63.ngrok.io"
 // Vue.prototype.$apiURL = "https://aaa-api.loca.lt"
 // Vue.prototype.$apiURL = "http://localhost:8080"
 
