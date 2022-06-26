7eb1fe0ef6e17ca39269d58c471a6ce24e0f13ea
Diego <dfsanchezme@gmail.com>
2022-06-26 10:01:36 -0500

    Updating scaffolds quote form

--- src/pages/Andamios.vue
+++ src/pages/Andamios.vue
@@ -17,40 +17,28 @@
         <h2 class="title mb-2">Cotizacion</h2>
         <p class="description">Andamios</p>
         <div class="row">
-          <div class="col-lg-6 m-auto">
-            <div class="row col-11 my-2 mx-auto">
-              <label class="col-lg-3 col-md-3 my-auto text-left">Base</label>
-              <input type="number" class="form-control col-3 col-lg-2 col-md-2 my-auto" v-model="quote.width"/>
-              <span class="description col-1 h5 my-auto py-0 pl-1">m</span>
-              <span class="description col-1 h2 my-auto py-0 mr-2 pl-1">&times;</span>
-              <input type="number" class="form-control col-3 col-lg-2 col-md-2 my-auto" v-model="quote.depth"/>
-              <span class="description col-1 h5 my-auto p-0">m</span>
-            </div>          
-            <div class="row col-11 my-2 mx-auto">
-              <label class="col-lg-3 col-md-3 col-sm-3 my-auto text-left"># Niveles</label>
-              <input type="number" class="form-control col-lg-2 col-md-3 col-sm-3 my-auto" v-model="quote.levels"/>
-              <n-checkbox v-model="quote.wheels" class="col-lg-4 col-md-4 col-sm-4 ml-auto mt-4 mt-lg-auto mt-md-auto mt-sm-auto" inline>¿Con ruedas?</n-checkbox>
-            </div>          
+          <div class="col-lg-6 m-auto">      
             <div class="row col-11 my-2">
-              <label class="col-lg-4 col-md-4 col-sm-4 col-6 my-auto text-left">Altura por nivel</label>
-              <input type="number" class="form-control col-4 col-lg-2 col-md-2 col-sm-2 my-auto" v-model="quote.levelHeight"/>
+              <label class="col-lg-4 col-md-4 col-sm-4 col-6 my-auto text-left">Base</label>
+              <input type="number" class="form-control col-4 col-lg-2 col-md-2 col-sm-2 my-auto" v-model="quote.base"/>
+              <span class="description col-1 h5 my-auto p-0">m</span>
+            </div> 
+            <div class="row col-11 my-2">
+              <label class="col-lg-4 col-md-4 col-sm-4 col-6 my-auto text-left">Altura</label>
+              <input type="number" class="form-control col-4 col-lg-2 col-md-2 col-sm-2 my-auto" v-model="quote.height"/>
               <span class="description col-1 h5 my-auto p-0">m</span>
             </div> 
           </div>
           <div class="col-lg-6 text-center m-auto">       
             <label class="my-auto text-left">Tipo</label>
             <div class="row">
-              <div class="col-lg-4 col-md-4">
+              <div class="col-lg-6 col-md-6">
                 <n-radio v-model="quote.type" label="1">Tipo 1</n-radio>
                 <img class="tipoAndamio" src="img/andamioTipo1.jpg" alt="Andamio Tipo 1" @click="selectTipo('1')"/>
               </div>
-              <div class="col-lg-4 col-md-4">
+              <div class="col-lg-6 col-md-6">
                 <n-radio v-model="quote.type" label="2">Tipo 2</n-radio>
                 <img class="tipoAndamio" src="img/andamioTipo2.png" alt="Andamio Tipo 2" @click="selectTipo('2')"/>
-              </div>
-              <div class="col-lg-4 col-md-4">
-                <n-radio v-model="quote.type" label="3">Tipo 3</n-radio>
-                <img class="tipoAndamio" src="img/andamioTipo3.jpg" alt="Andamio Tipo 3" @click="selectTipo('3')"/>
               </div>
             </div>
           </div>
@@ -109,11 +97,8 @@ export default {
     return {
       quote: {
         userEmail: "",
-        width: 5.0,
-        depth: 5.0,
-        levels: 8,
-        levelHeight: 2.0,
-        wheels: false,
+        base: 5.0,
+        height: 5.0,
         type: '2',
         done: false
       },
@@ -133,7 +118,7 @@ export default {
     registerQuote: function(quote){
       this.validateEmail()
       if(this.validEmail){
-        if(quote.width > 0 && quote.depth > 0 && quote.levels > 0 && quote.levelHeight > 0){
+        if(quote.base > 0 && quote.height > 0){
           axios.post(this.$apiURL+"/scaffoldQuote", quote)
           .then((response) => {
             if(response.data){
@@ -167,10 +152,8 @@ export default {
     },
     restartFields(){
       this.quote.userEmail = ""
-      this.quote.width = 5
-      this.quote.depth = 5
-      this.quote.levels = 2
-      this.quote.wheels = false
+      this.quote.base = 5.0
+      this.quote.height = 5.0
       this.quote.type = '2'
       this.hideNotifications()
     },
